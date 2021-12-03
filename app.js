017d52f0cdefe7902b27aa709cf4329d750b4769
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-02 18:07:01 -0800

    full html structure css styling, relevent dom elements grabbed in js

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 // import functions and grab DOM elements
+const density = document.getElementById('density');
+const elevation = document.getElementById('elevation');
+const weather = document.getElementById('weather');
+
+const structure1 = document.getElementById('structure1');
+const structure2 = document.getElementById('structure2');
+const structure3 = document.getElementById('structure3');
+const triangle1 = document.getElementById('triangle1');
+const triangle2 = document.getElementById('triangle2');
+const triangle3 = document.getElementById('triangle3');
+const weather1 = document.getElementById('weather1');
+const weather2 = document.getElementById('weather2');
+const weather3 = document.getElementById('weather3');
+
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
+
+const citySlogan = document.getElementById('city-slogan');
+const populationStats = document.getElementById('population-stats');
+const elevationStats = document.getElementById('elevation-stats');
+const weatherStats = document.getElementById('weather-stats');
+
 
 // let state
 
