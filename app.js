8e4f375d459580a89462be7b1a2db0fa7e5ffb3e
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-02 19:04:49 -0800

    eventListener set for density elevation weather select elements, displays corresponding divs on change

--- app.js
+++ app.js
@@ -6,40 +6,17 @@ const weather = document.getElementById('weather');
 const sloganInput = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
 
-const structure1 = document.getElementById('structure1');
-const structure2 = document.getElementById('structure2');
-const structure3 = document.getElementById('structure3');
-const triangle1 = document.getElementById('triangle1');
-const triangle2 = document.getElementById('triangle2');
-const triangle3 = document.getElementById('triangle3');
-const weather1 = document.getElementById('weather1');
-const weather2 = document.getElementById('weather2');
-const weather3 = document.getElementById('weather3');
+// const triangle1 = document.getElementById('triangle1');
+// const triangle2 = document.getElementById('triangle2');
+// const triangle3 = document.getElementById('triangle3');
+// const weather1 = document.getElementById('weather1');
+// const weather2 = document.getElementById('weather2');
+// const weather3 = document.getElementById('weather3');
 
 const citySlogan = document.getElementById('city-slogan');
 const populationStats = document.getElementById('population-stats');
 const elevationStats = document.getElementById('elevation-stats');
 const weatherStats = document.getElementById('weather-stats');
-
-
-console.log(density,
-elevation,
-weather,
-sloganInput,
-sloganButton,
-structure1,
-structure2,
-structure3,
-triangle1,
-triangle2,
-triangle3,
-weather1,
-weather2,
-weather3,
-citySlogan,
-populationStats,
-elevationStats,
-weatherStats);
 
 // let state
 let sloganArray = [];
@@ -49,15 +26,21 @@ let currentWeather = 0;
 
 // set event listeners 
 density.addEventListener('change', () => {
-  let id = density.value;
+    let struct = document.getElementById(`structure${density.value}`);
+    struct.classList.remove('visibility');
+    console.log(struct);
 });
 
 elevation.addEventListener('change', () => {
-
+    let elev = document.getElementById(`elevation${elevation.value}`);
+    elev.classList.remove('visibility');
+    console.log(elev);
 });
 
 weather.addEventListener('change', () => {
-
+    let weath = document.getElementById(`weather${weather.value}`);
+    weath.classList.remove('visibility');
+    console.log(weath);
 });
   // get user input
   // use user input to update state 
