cafc15fb82a1be2e5ff87766596ea724459c546c
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-02 18:14:38 -0800

    validated dom elements grabbed, let state, eventListener setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const density = document.getElementById('density');
 const elevation = document.getElementById('elevation');
 const weather = document.getElementById('weather');
 
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
+
 const structure1 = document.getElementById('structure1');
 const structure2 = document.getElementById('structure2');
 const structure3 = document.getElementById('structure3');
@@ -13,18 +16,49 @@ const weather1 = document.getElementById('weather1');
 const weather2 = document.getElementById('weather2');
 const weather3 = document.getElementById('weather3');
 
-const sloganInput = document.getElementById('slogan-input');
-const sloganButton = document.getElementById('slogan-button');
-
 const citySlogan = document.getElementById('city-slogan');
 const populationStats = document.getElementById('population-stats');
 const elevationStats = document.getElementById('elevation-stats');
 const weatherStats = document.getElementById('weather-stats');
 
 
+console.log(density,
+elevation,
+weather,
+sloganInput,
+sloganButton,
+structure1,
+structure2,
+structure3,
+triangle1,
+triangle2,
+triangle3,
+weather1,
+weather2,
+weather3,
+citySlogan,
+populationStats,
+elevationStats,
+weatherStats);
+
 // let state
+let sloganArray = [];
+let currentPopulation = 0;
+let currentElevation = 0;
+let currentWeather = 0;
 
 // set event listeners 
+density.addEventListener('change', () => {
+  let id = density.value;
+});
+
+elevation.addEventListener('change', () => {
+
+});
+
+weather.addEventListener('change', () => {
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
