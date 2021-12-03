e9813b89f68cc997c0ef9435514d9faa652e4486
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-02 22:51:31 -0800

    app.js comment cleanup

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import functions and grab DOM elements
 import { createCountsString } from '../utils.js';
 
 const density = document.getElementById('density');
@@ -8,12 +7,6 @@ const weather = document.getElementById('weather');
 const sloganInput = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
 
-// const triangle1 = document.getElementById('triangle1');
-// const triangle2 = document.getElementById('triangle2');
-// const triangle3 = document.getElementById('triangle3');
-// const weather1 = document.getElementById('weather1');
-// const weather2 = document.getElementById('weather2');
-// const weather3 = document.getElementById('weather3');
 const statistics = document.getElementById('statistics');
 
 const citySlogan = document.getElementById('city-slogan');
@@ -21,19 +14,17 @@ const populationStats = document.getElementById('population-stats');
 const elevationStats = document.getElementById('elevation-stats');
 const weatherStats = document.getElementById('weather-stats');
 
-// let state
+
 let sloganArray = [];
 let currentPopulation = 0;
 let currentElevation = 0;
 let currentWeather = 0;
 
-// set event listeners 
 density.addEventListener('change', () => {
     currentPopulation++;
     console.log(currentPopulation);
     let struct = document.getElementById(`structure${density.value}`);
     struct.classList.remove('visibility');
-    // console.log(struct);
     displayStats();
 });
 
@@ -41,7 +32,6 @@ elevation.addEventListener('change', () => {
     currentElevation++;
     let elev = document.getElementById(`elevation${elevation.value}`);
     elev.classList.remove('visibility');
-    // console.log(elev);
     displayStats();
 });
 
@@ -49,7 +39,6 @@ weather.addEventListener('change', () => {
     currentWeather++;
     let weath = document.getElementById(`weather${weather.value}`);
     weath.classList.remove('visibility');
-    // console.log(weath);
     displayStats();
 });
 
@@ -67,7 +56,6 @@ function displayStats() {
     statistics.append(p);
     const statsCount = createCountsString(currentPopulation, currentElevation, currentWeather);
     p.textContent = statsCount;
-    // console.log(statsCount);
 }
 
 function displaySlogans() {
